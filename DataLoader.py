8aaf7d52809a8c8af67a704cb150c340964d8c5e
Guy-El Fridman <guy-elf@campus.technion.ac.il>
2021-12-20 19:32:11 +0200

    Started working on getting all metrics.

--- DataLoader.py
+++ DataLoader.py
@@ -1,17 +1,8 @@
-from prometheus_api_client import PrometheusConnect
-from prometheus_api_client.metric_range_df import MetricRangeDataFrame
-from prometheus_api_client.utils import parse_datetime
 from datetime import timedelta
-import pandas as pd
-
+from PrometheusClient import PrometheusClient
 
 if __name__ == '__main__':
-    operate_first_token = "sha256~Cg4oQ5qOWfUWNuWisJOuEyNOsD4A6Yed-SRxG-8APKM"
-    thanos_url = 'https://thanos-query-frontend-opf-observatorium.apps.smaug.na.operate-first.cloud'
-    pc = PrometheusConnect(
-        url=thanos_url,
-        headers={"Authorization": f"Bearer {operate_first_token}"},
-        disable_ssl=False
-    )
-    pd.DataFrame(pc.all_metrics(), columns={"metrics"})
-    pc.get_current_metric_value("http_requests_total")
+    pc = PrometheusClient()
+    df_all = pc.getAllMetrics(time_range=timedelta(seconds=5))
+    pass
+
--- PrometheusClient.py
+++ PrometheusClient.py
@@ -3,26 +3,36 @@ from prometheus_api_client.metric_range_df import MetricRangeDataFrame
 from datetime import timedelta, datetime
 import pandas as pd
 
-OPERATE_FIRST_TOKEN = "sha256~Cg4oQ5qOWfUWNuWisJOuEyNOsD4A6Yed-SRxG-8APKM"
+OPERATE_FIRST_TOKEN = "sha256~_2MH2RN9JX1-a39wTHPtiGflHN4PSLyMJOvMe__H0lQ"
 THANOS_URL = "https://thanos-query-frontend-opf-observatorium.apps.smaug.na.operate-first.cloud"
 
 
 class PrometheusClient:
     def __init__(self):
+        self.end_time = datetime.now()
         self.pc = PrometheusConnect(
             url=THANOS_URL,
             headers={"Authorization": f"Bearer {OPERATE_FIRST_TOKEN}"},
             disable_ssl=False)
 
+    def getAllMetrics(self, time_range: timedelta):
+        df_all = pd.DataFrame()
+        for metric in self.pc.all_metrics():
+            df = self.getRangeData(metric, time_range)
+            if df is not None:
+                df_all.join(df)
+        return df_all
+
     def getRangeData(self, metric, time_range: timedelta):
-        end_time = datetime.now()
-        start_time = end_time - time_range
+        start_time = self.end_time - time_range
         metric_data = self.pc.get_metric_range_data(
             metric,  # metric name and label config
             start_time=start_time,  # datetime object for metric range start time
-            end_time=end_time,  # datetime object for metric range end time
+            end_time=self.end_time,  # datetime object for metric range end time
             chunk_size=time_range  # timedelta object for duration of metric data downloaded in one request
         )
+        if not metric_data:
+            return None
         metric_df = MetricRangeDataFrame(metric_data)
         metric_df.index = pd.to_datetime(metric_df.index.tolist(), unit="s", utc=True)
         return metric_df
