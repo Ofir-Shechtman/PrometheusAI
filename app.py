cb49c856892a98c3e49a9d88b60e68ac46190e4f
Ben Lugasi <benlugasi@campus.technion.ac.il>
2022-01-30 16:27:36 +0200

    app.py added

---
+++ app.py
@@ -0,0 +1,9 @@
+from datetime import timedelta
+from PrometheusClient import PrometheusClient
+
+if __name__ == '__main__':
+    pc = PrometheusClient()
+    for df in pc.getAllMetrics(time_range=timedelta(seconds=5)):
+        print(df)
+        # TODO: read alog's implementations to find out how we need to set the dataloader
+
