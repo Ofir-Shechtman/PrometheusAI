9e04199d484d133dcc66b26ca361040dbb2a6a9c
ofir511996 <ofir511996@gmail.com>
2022-02-05 16:54:58 +0200

    add preprocess_thanos.py to create noobaa-mgmt file

--- DataLoader.py
+++ DataLoader.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
-from PrometheusClient import PrometheusClient
+from PrometheusClient import PrometheusClient, ThanosConnect
+
+
 
 if __name__ == '__main__':
     pc = PrometheusClient()
+    row_count = 0
     for df in pc.getAllMetrics(time_range=timedelta(seconds=5)):
-        print(df)
+        print(df["__name__"][0])
         # TODO: read alog's implementations to find out how we need to set the dataloader
-
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-OPERATE_FIRST_TOKEN = "sha256~RT0D8Zyftqt80pXThIZsGWfnO9cpuer7KQ_Q4Hw7RQY"
+# token-gen https://www.operate-first.cloud/apps/content/observatorium/thanos/thanos_programmatic_access.html
+OPERATE_FIRST_TOKEN = "sha256~GluQRHbQ0g9Ms8A34uJL3tLJghDaiuc8tlu_FUc6a9Q"
 THANOS_URL = "https://thanos-query-frontend-opf-observatorium.apps.smaug.na.operate-first.cloud"
